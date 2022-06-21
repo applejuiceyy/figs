bb19d2a0953203727b3f43ebfd480b760d0b29f2
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2022-06-21 08:49:09 +0000

    Update gh-pages to output generated at a800eec

--- _app/pages/_category_/index.svelte-8cbca28c.js
+++
@@ -1,9 +0,0 @@
-import{S as $,i as C,s as w,x as c,k as j,y as f,m as y,z as p,g as D,r as u,p as _,C as d,d as M}from"../../chunks/index-26185746.js";import{r as g}from"../../chunks/Article.svelte_svelte_type_style_lang-2cd13b43.js";import{C as S}from"../../chunks/CategoryDocs-2e732474.js";import{M as h}from"../../chunks/MetaTags-4dff3baf.js";import"../../chunks/CategoryItemDocs-5bfbb640.js";function k(e){let t,o,s,r;return t=new h({props:{title:e[1],description:e[0].map(l).join(`
-
-
-`)}}),s=new S({props:{docs:e[0],name:e[1]}}),{c(){c(t.$$.fragment),o=j(),c(s.$$.fragment)},l(n){f(t.$$.fragment,n),o=y(n),f(s.$$.fragment,n)},m(n,a){p(t,n,a),D(n,o,a),p(s,n,a),r=!0},p(n,[a]){const m={};a&2&&(m.title=n[1]),a&1&&(m.description=n[0].map(l).join(`
-
-
-`)),t.$set(m);const i={};a&1&&(i.docs=n[0]),a&2&&(i.name=n[1]),s.$set(i)},i(n){r||(u(t.$$.fragment,n),u(s.$$.fragment,n),r=!0)},o(n){_(t.$$.fragment,n),_(s.$$.fragment,n),r=!1},d(n){d(t,n),n&&M(o),d(s,n)}}}let A=function({params:{category:e}}){return e in g?{props:{docs:g[e],name:e}}:{status:404}};const l=e=>e.name+`
-
-`+e.description;function q(e,t,o){let{docs:s}=t,{name:r}=t;return e.$$set=n=>{"docs"in n&&o(0,s=n.docs),"name"in n&&o(1,r=n.name)},[s,r]}class E extends ${constructor(t){super(),C(this,t,q,k,w,{docs:0,name:1})}}export{E as default,A as load};
