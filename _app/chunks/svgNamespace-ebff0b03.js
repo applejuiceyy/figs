6072acb81968377d6ebab0356316d9e0ba23b909
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2022-06-25 16:35:14 +0000

    Update gh-pages to output generated at a2a486d

---
+++ _app/chunks/svgNamespace-ebff0b03.js
@@ -0,0 +1,1 @@
+var a="http://www.w3.org/2000/svg";export{a as n};
--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
 const build = [
-  "/figs/_app/start-674599f5.js",
-  "/figs/_app/pages/__layout.svelte-32789ff4.js",
-  "/figs/_app/assets/pages/__layout.svelte-fb143380.css",
+  "/figs/_app/start-a6907914.js",
+  "/figs/_app/pages/__layout.svelte-fbefff3b.js",
+  "/figs/_app/assets/pages/__layout.svelte-126fb55f.css",
   "/figs/_app/assets/cheese-9bdf176f.png",
   "/figs/_app/error.svelte-130067e2.js",
-  "/figs/_app/pages/_category_/_item_/_property_.svelte-69cc7268.js",
-  "/figs/_app/pages/_category_/_item_/index.svelte-8ef81d78.js",
-  "/figs/_app/pages/_category_/index.svelte-07205317.js",
-  "/figs/_app/pages/index.svelte-b747e377.js",
+  "/figs/_app/pages/_category_/_item_/_property_.svelte-1b4e3866.js",
+  "/figs/_app/pages/_category_/_item_/index.svelte-66599481.js",
+  "/figs/_app/pages/_category_/index.svelte-928e9e79.js",
+  "/figs/_app/pages/index.svelte-d4b1ccda.js",
   "/figs/_app/chunks/index-8bf6b353.js",
   "/figs/_app/chunks/paths-396f020f.js",
   "/figs/_app/chunks/preload-helper-a6ef5122.js",
   "/figs/_app/chunks/Article.svelte_svelte_type_style_lang-99973d4f.js",
   "/figs/_app/assets/Article.svelte_svelte_type_style_lang-da5e2cae.css",
-  "/figs/_app/chunks/MetaTags-8c74603f.js",
+  "/figs/_app/chunks/svgNamespace-ebff0b03.js",
+  "/figs/_app/chunks/MetaTags-cdc41b4c.js",
   "/figs/_app/assets/stone1-97f2a8a7.ogg",
   "/figs/_app/assets/stone2-98a5c319.ogg",
   "/figs/_app/assets/stone3-19273f0e.ogg",
@@ -25,9 +26,9 @@ const build = [
   "/figs/_app/assets/stone5-67b1e504.ogg",
   "/figs/_app/assets/stone6-2b65e1bd.ogg",
   "/figs/_app/assets/hammer-ccd8d2cd.svg",
-  "/figs/_app/chunks/CategoryItemDocs-e63e2a25.js",
-  "/figs/_app/chunks/CategoryDocs-894db39f.js",
-  "/figs/_app/chunks/gravity-ee11c181.js"
+  "/figs/_app/chunks/CategoryItemDocs-36ae1fd5.js",
+  "/figs/_app/chunks/CategoryDocs-c5cf0586.js",
+  "/figs/_app/chunks/gravity-a6de735c.js"
 ];
 const files = [
   "/figs/favicon.png"
@@ -718,7 +719,7 @@ const prerendered = [
   "/figs/misc/SoundAPI/playSound/",
   "/figs/misc/ParticleAPI/addParticle/"
 ];
-const version = "1656115359783";
+const version = "1656174902793";
 const worker = self;
 const CACHE_NAME = `A;static-cache-${version}`;
 const to_cache = build.concat(files).concat(prerendered);
