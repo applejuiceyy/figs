6cb373dc8057e8cf1f905d3c6c9d1f6676136cb2
applejuiceyy <30875598+applejuice-wastaken@users.noreply.github.com>
2022-07-15 08:59:23 +0100

    more bugfixing?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,1 @@
-<script>
-    import { MetaTags } from "svelte-meta-tags";
-</script>
-
-<MetaTags title="FIGS!!!" description="The thingy applejuice made because they're bored!\n(Now Rewritten :flushed:)"/>
-
 figs
