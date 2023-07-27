36837d0b00136d9b4dc6672a817f4be544bc36f3
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2023-07-27 17:42:17 +0100

    do you want to work now

--- src/routes/[version]/meta/docs.json/+server.ts
+++ src/routes/[version]/meta/docs.json/+server.ts
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit";
 import versions from "docs:all";
 import latest from "docs:latest";
-
+export const prerender = true
 /** @type {import('./$types').RequestHandler} */
 export async function GET({params}) {
     return json(await versions[params.version === "latest" ? latest : params.version]());
--- src/routes/meta/docs.json/+server.ts
+++ src/routes/meta/docs.json/+server.ts
@@ -1,6 +1,6 @@
 import { json } from "@sveltejs/kit";
 import versions from "docs:all";
-
+export const prerender = true
 export async function GET() {
     return json(Object.keys(versions));
 }
